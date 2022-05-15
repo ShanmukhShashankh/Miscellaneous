8f42b6936a12e11f84a6c77ff85d3ec688d246f7
ShanmukhShashankh <vemula.shanmukhshashankh@gmail.com>
2022-05-15 22:28:06 +0530

    3x+1

--- 3x+1.py
+++ 3x+1.py
@@ -17,5 +17,18 @@ while x>1:
 xaxis = np.arange(0, len(y))
 y = np.array(y)
 
+max = int(y.max())
+index = np.where(y==max)
+xmax = int(xaxis[index])
+
+end = int(y.min())
+end_index = np.where(y==end)
+xmin = int(xaxis[end_index])
+
+
 plt.plot(xaxis,y)
+plt.annotate(f'({int(xaxis[0])}, {int(y[0])})', xy=(int(xaxis[0]), int(y[0])))
+plt.annotate(f'({xmax}, {max})', xy=(xmax, max))
+plt.annotate(f'({xmin}, {end})', xy=(xmin, end))
 plt.show()
+
